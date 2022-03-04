0fb52a06a15960cc4694d7ac164d45803a212f91
Matthew Gale <matthew.gale@vodafone.com>
2022-03-04 18:19:24 +0000

    Added doc strings

--- item.py
+++ item.py
@@ -1,6 +1,9 @@
 class Item:
     #all prices are in pence as storing floats causes issues
     def __init__(self, name, price, quantity):
+        """
+        Create an instance of the item class. Initialises attributes and calls relevant methods.
+        """
         #double underscore ensures that the attributes are private, meaning they cannot be accessed or modified from outside the class
         self.__name = name
         self.__stored_price = int(float(price)*100)          
@@ -8,42 +11,61 @@ class Item:
         self.__quantity = int(quantity)
 
     def __repr__(self):
-        #used to update csv file
+        """
+        Defines how an item is represented
+        """
         string =  self.__name+" - "+self.__display_price+" x "+str(self.__quantity)
         return string     
 
     def __str__(self):
-        #defines how the item class is formatted when printed to the screen
+        """
+        Defines how an item is represented when converted to a string
+        """
         return self.__name
 
     def __eq__(self, other):
-        #defines behaviour when two instances of the class are compared against each other
+        """
+        Defines behaviour when two instances of the class are compared against each other
+        """
         return self.__name == other   
 
     def __sub__(self,other) :
+        """
+        Defines behaviour when two instances of the class are subtracted from each other
+        """
         self.__quantity -= other.get_quantity()
         return self
 
     def __lt__(self, other):
-        #compares whether one instance of the item class is less than the other
+        """
+        Compares whether one instance of the item class is less than the other
+        """
         return self.__quantity < other.get_quantity()
 
     def __gt__(self, other):
-        #compares whether one instance of the item class is greater than the other
+        """
+        Compares whether one instance of the item class is greater than the other
+        """
         print(repr(self))
         print(other)
         return self.__quantity > other.get_quantity()
 
     def __le__(self, other):
-        #compares whether one instance of the item class is less than or equal to the other
+        """
+        Compares whether one instance of the item class is less than or equal to the other
+        """
         return self.__quantity <= other.get_quantity()
 
     def __ge__(self, other):
-        #compares whether one instance of the item class is greater than or equal to the other
+        """
+        Compares whether one instance of the item class is greater than or equal to the other
+        """
         return self.__quantity >= other.get_quantity()    
 
     def get_summary(self):
-        #used to update csv file
+        """
+        Used to update csv file
+        """
         string =  self.__name+","+str(float(self.__stored_price)/100)+","+str(self.__quantity)
         return string     
 
